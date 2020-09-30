1344abbb623d495276bedacd5593021783e07e21
Leonard Melnik <leonard@onedr.io>
2020-09-30 10:51:19 -0400

    Sending data from backend back to frontend

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('joinGroup', (data) => {
       console.log("Joining group: ", data.group)
-  })
+      socket.emit('getGroup', {
+            name : data.group
 
-  
+      });
+      console.log("Sent out group")
+  })
 });
 
 http.listen(80, () => {
-  console.log('listening on *:3000');
+  console.log('listening on *:80');
 });
