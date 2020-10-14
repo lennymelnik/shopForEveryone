107b369ec8243e9ef05329f59e1dca21b031fde6
Leonard Melnik <leonard@onedr.io>
2020-10-13 20:47:39 -0400

    Added live changes and mongodb

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # shopForEveryone
 A list app where people in certain groups can join. (Familys, companies) So if someone goes shopping, they can see what others need and can get it for them
+
+# Installation/usage
+Webapp and APK for android9
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const { group } = require('console');
+const { query } = require('express');
 
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var url = "mongodb://projectclout.com:27017/mydb";
+var uniqid = require('uniqid')
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/website/index.html');
@@ -12,45 +18,136 @@ var groupOBJ = {
   name : "Bobs food",
   items : ["Chocolate Milk", "Nuggie", "Tendies", "Happiness"]
 }
+MongoClient.connect(url, function(err,db){
+  if (err) throw err;
+  dbo = db.db("smallProjects");
+  io.on('connection', (socket) => {
 
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  //User wants to see list of a group
-  socket.on('joinGroup', (data) => {
-      console.log("Joining group: ", data.group)
-      //Send group information to user
-      socket.emit('getGroup', groupOBJ);
-     
-  })
-  socket.on('addItem', (data) => {
-    if(data != ""){
-      groupOBJ.items.push(data)
-    }
     
-    //Send group information to user
-    socket.emit('getGroup', groupOBJ);
-   
-  })
-
-
-  //When a person clicks on got items
-  socket.on("gottenItems",(data) =>{
-    console.log("removing item")
-    console.log(typeof data)
-    for(i=0;i<data.items.length;i++){
-      
-      //Remove item from array based on index given in data pass
-      groupOBJ.items.splice(data.items[i], 1);
-      
-    }
-    // Once we remove item for items we must send the updated list
-    socket.emit('getGroup', groupOBJ);
+    console.log('a user connected');
+    //User wants to see list of a group
+    socket.on('joinGroup', (data) => {
+        console.log("Joining group: ", data.group)
+        query1 = {
+          name : data.group
+        }
+        dbo.collection("shopForAll").find(query1).toArray(function(err, result) {
+          if (err) throw err;
+          socket.emit('getGroup', result[0]);
+          
+          dbo.collection("chats").find(query1).toArray(function(err, resu) {
+              console.log("starting to watch", resu[0])
   
-  })
-});
+            var local = resu
+          // Define change stream
+  
+          var changeStream = dbo.collection("shopForAll").watch(query);
+          // start listen to changes
+          changeStream.on("change", function(event) {
+            dbo.collection("shopForAll").find(query1).toArray(function(err, result) {
+              if (err) throw err;
+              socket.emit('getGroup', result[0]);
+            })
+          })
+        })
+        //Send group information to user
+      })
+        
+     
+            
+
+    })
+
+    socket.on('createGroup', (data) =>{
+
+      var query = {
+        id : uniqid(),
+        name : data.name,
+        key : data.key,
+        list : [],
+        people : []
+      }
+      dbo.collection("shopForAll").insertOne(query, function(err, result) {
+        if (err) throw err;
+        console.log("Added a new group")
+        
+
+      });
+
+    })
+
+  
+    socket.on('addItem', (data) => {
+      if(data.item != ""){
+        hquery = {
+          id : data.id
+        }
+        newValues = {
+          $push: {
+          list: data.item
+          }
+      }
+
+        dbo.collection("shopForAll").updateOne(hquery, newValues, function(err, result) {
+          if (err) throw err;
+          console.log("Added a new group")
+          dbo.collection("shopForAll").find(hquery).toArray(function(err, result) {
+            if (err) throw err;
+            socket.emit('getGroup', result[0]);
+          })
+       
+  
+        });
+      
+      }
+      
+      //Send group information to user
+      
+    
+    })
 
 
-http.listen(80, () => {
-  console.log('listening on *:80');
-});
+    //When a person clicks on got items
+    socket.on("gottenItems",(data) =>{
+      
+      console.log("removing item")
+      console.log(typeof data)
+      hquery = {
+        id : data.groupId
+      }
+      dbo.collection("shopForAll").find(hquery).toArray(function(err, result) {
+        if (err) throw err;
+        var groupInfo = result[0]
+        console.log("being passed", data.items)
+        for(i=0;i<data.items.length;i++){
+        
+          //Remove item from array based on index given in data pass
+          groupInfo.list.splice(data.items[i], 1);
+          
+        }
+        newValues = {
+          $set: {
+          list: groupInfo.list
+          }
+        }
+
+        dbo.collection("shopForAll").updateOne(hquery, newValues, function(err, result) {
+          if (err) throw err;
+          console.log("info",groupInfo)
+          // Once we remove item for items we must send the updated list
+          socket.emit('getGroup', groupInfo);
+        })
+     
+       
+
+      })
+      
+    
+    })
+  });
+
+
+  http.listen(80, () => {
+    console.log('listening on *:80');
+  });
+})
